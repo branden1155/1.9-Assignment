2c9d94dd27c33840b66babdac306acb971965ee2
branden1155 <brandenp119@gmail.com>
2022-12-05 23:32:11 -0500

    updated

--- src/components/blogPost/blogpostThree.js
+++ src/components/blogPost/blogpostThree.js
@@ -1,19 +1,19 @@
 import React from "react";
 import './blogStyle.css'
-import VueImage from "../../images/vue-image.jpg"
+import APIImage from "../../images/apiImage.png"
 import { Link } from "react-router-dom";
 
 function BlogThree(){
     return(
         <div className="blogCard">
     <a href="#">
-        <img className="image" src={VueImage} alt="blogImage" />
+        <img className="image" src={APIImage} alt="blogImage" />
     </a>
     <div className="blogcardInfo">
         <a href="#">
-            <h5 className="blogcardTitle">What is Vue and how does it really work?</h5>
+            <h5 className="blogcardTitle">Learning what's in Important when it comes to API's</h5>
         </a>
-        <p className="blogcardDetails">As I continue my learning adventure through learning librarys such as ReactJS and Tailwind, I constantly here of a also popular library called Vue. What is it, and how does it work?</p>
+        <p className="blogcardDetails">During this week, I watched many videos explaing the the different important aspects of React API's, and today I'll show you what to look for, and how to utilize them when you need them!</p>
         <Link to="/BlogpageThree" className="readmoreTag">Read more</Link>
     </div>
 </div>
--- src/pages/blogPages/blogpageThree.js
+++ src/pages/blogPages/blogpageThree.js
@@ -1,15 +1,15 @@
 import React from "react";
 import './blogpageStyles.css'
-import VueImage from "../../images/vue-image.jpg"
+import APIImage from "../../images/apiImage.png"
 
 function BlogThree() {
     return(
         <div className="blogoneContainer">
             <div className="blogoneHero">
-                <img className="blogoneImage" alt="mongoDB Image" src={VueImage}></img>
+                <img className="blogoneImage" alt="mongoDB Image" src={APIImage}></img>
                 <div className="blogoneInfo">
-                    <span className="blogoneTitle">What is Vue and how does it really work?</span>
-                    <span className="blogoneDetails">As I continue my learning adventure through learning librarys such as ReactJS and Tailwind, I constantly here of a also popular library called Vue. What is it, and how does it work? Vue is similar to React in a way. Allowing for quick and precise front-end creations utilizing HTML and CSS. While Vue and React share a lot of similiarities, Vue seems to share a much more in-depth community with more documentation following/backing it up. Personally, I'm excited to learn more about Vue and the ways of its library!</span>
+                    <span className="blogoneTitle">Learning what's in Important when it comes to API's</span>
+                    <span className="blogoneDetails">As I continue my learning adventure through learning React API's. One of the most important idea/tip that has really helped me in my adventure is Project folder Structuing. When I first started attempting to write API's I created my folders without a care in the world. Files started getting jumbled up into a massive mess. Thats where Folder Structuing comes in. This allows you to evenly organize and space out your code into specific files. The most common way that people do this is by using a component-centric structure. Creating Component like structures similar to React coding. This is a great help when you run into coding bugs, or errors. Having a good file structure helps you locate and fix them with ease. Im eager add this into my coding practice! </span>
                     <a href="/" className="backLink">Back</a>
                 </div>
             </div>
